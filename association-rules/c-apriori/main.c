271e02ae040b830a38164ea6162da4a76b9dee8a
uno <st9007a@gmail.com>
2017-03-09 15:32:40 +0800

    fix parse transaction

--- association-rules/c-apriori/main.c
+++ association-rules/c-apriori/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-char** parseTransaction(char*);
+char** parse_transaction(char*);
 
 int main(int argc, char *argv[]) {
 
   float sup;
   float conf;
 
-  FILE* inputFile;
+  FILE* input_file;
   char* line;
   size_t len = 0;
   ssize_t read;
@@ -28,14 +28,38 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  inputFile = fopen("input.txt", "r");
-  if (inputFile == NULL) {
+  input_file = fopen("input.txt", "r");
+  if (input_file == NULL) {
     exit(EXIT_FAILURE);
   }
 
-  while ((read = getline(&line, &len, inputFile)) != 1) {
-    transaction = parseTransaction(line);
+  i = 0;
+  while ((read = getline(&line, &len, input_file)) != -1) {
+    line[strlen(line) - 1] = '\0';
+    transaction = parse_transaction(line);
+    i++;
   }
 
+  sup *= i;
+
+  fclose(input_file);
+
   return 0;
+}
+
+char** parse_transaction(char* line) {
+  char** tran;
+  char* line_cp = (char*)malloc(strlen(line) + 1);
+  char* item;
+  int i = 0;
+  strcpy(line_cp, line);
+
+  item = strtok(line_cp, " ");
+  while (item != NULL) {
+    i++;
+    item = strtok(NULL, " ");
+  }
+  tran = (char**)malloc(i * sizeof(char*));
+
+  return tran;
 }

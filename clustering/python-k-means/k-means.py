7212b3b54995ea78ad3ac8817e17486d26d1a192
uno <st9007a@gmail.com>
2017-03-25 20:25:33 +0800

    feat: parse data list

--- clustering/python-k-means/k-means.py
+++ clustering/python-k-means/k-means.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 from argparse import ArgumentParser
 
 class Cluster:
@@ -10,12 +11,32 @@ class Cluster:
 
 def main():
 
+    input_file = None
     parser = ArgumentParser()
-    parser.add_argument("--file", "-f", help="input the file name you want to clustering")
+
+    parser.add_argument('--file', '-f', help='input the file name you want to clustering')
     args = parser.parse_args()
 
-    if args.file:
-        print args.file
+    if args.file is None:
+        print 'Not input file.'
+        sys.exit(0)
+
+    try:
+        input_file = open(args.file, 'r')
+    except(OSError, IOError) as e:
+        print 'File not found.'
+
+    raw_data_list = [elem.rstrip('\r\n') for elem in input_file.readlines()]
+
+    data_list = []
+    for coordinate in raw_data_list:
+        x = coordinate.split(' ')[0]
+        y = coordinate.split(' ')[1]
+        data_list.append({ 'x': x, 'y': y })
+
+    print data_list
+
+    input_file.close()
 
 if __name__ == '__main__':
     main()

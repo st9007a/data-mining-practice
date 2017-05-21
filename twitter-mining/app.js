625a65e7248f2eba82d888beb03ededa7e3ac57d
uno <st9007a@gmail.com>
2017-05-21 16:22:24 +0800

    trends collect

--- twitter-mining/app.js
+++ twitter-mining/app.js
@@ -1,27 +1,13 @@
-const ArgumentParser = require('argparse').ArgumentParser
 const fs = require('fs')
 const twit = require('twit')
 
-const parser = new ArgumentParser({
-  addHelp: true,
-  description: 'Default mining sample tweets.',
-  version: '0.0.1',
-})
 const t = new twit(require('./config.json'))
-const twitStream = t.stream('statuses/sample')
 
-parser.addArgument(['-f', '--filter'], {help: 'Get filter tweets'})
+const getTrends = () => t.get('trends/place', {id: 1}, (err, data, res) => {
+  if (err) throw err
+  console.log('Get trends at ' + new Date())
+  fs.appendFileSync('trends.txt', JSON.stringify(data) + '\n')
+})
 
-const args = parser.parseArgs()
-
-if (!args.filter) {
-  twitStream.on('tweet', (tweet) => {
-    console.log(tweet)
-    fs.appendFileSync('sample.tweets.txt', JSON.stringify(tweet) + '\n', 'utf-8')
-  })
-}
-else {
-  const filter = JSON.parse(args.filter)
-  console.log('filter: ' + JSON.stringify(filter))
-}
-
+getTrends()
+setInterval(() => getTrends(), 1000 * 60 * 10)

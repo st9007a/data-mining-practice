0e063bb4c2d226f134fe2e8152d13fbd4a0d0de6
uno <st9007a@gmail.com>
2017-05-21 10:23:49 +0800

    add args parser

--- twitter-mining/app.js
+++ twitter-mining/app.js
@@ -1,10 +1,26 @@
+const ArgumentParser = require('argparse').ArgumentParser
 const fs = require('fs')
 const twit = require('twit')
+
+const parser = new ArgumentParser({
+  addHelp: true,
+  version: '0.0.1',
+})
 const t = new twit(require('./config.json'))
 const twitStream = t.stream('statuses/sample')
 
-twitStream.on('tweet', (tweet) => {
-  console.log(tweet)
-  fs.appendFileSync('tweets.txt', JSON.stringify(tweet) + '\n', 'utf-8')
-})
+parser.addArgument(['-f', '--filter'], {help: 'Get filter tweets'})
+
+const args = parser.parseArgs()
+
+if (!args.filter) {
+  twitStream.on('tweet', (tweet) => {
+    console.log(tweet)
+    fs.appendFileSync('tweets.txt', JSON.stringify(tweet) + '\n', 'utf-8')
+  })
+}
+else {
+  const filter = JSON.parse(args.filter)
+  console.log('filter: ' + JSON.stringify(filter))
+}
 

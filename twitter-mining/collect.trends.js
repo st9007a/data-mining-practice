692df56939b720c055db0877d5654f736ed812a5
uno <st9007a@gmail.com>
2017-05-21 16:31:09 +0800

    rename and refactor data format

--- twitter-mining/collect.trends.js
+++ twitter-mining/collect.trends.js
@@ -3,9 +3,11 @@ const twit = require('twit')
 
 const t = new twit(require('./config.json'))
 
-const getTrends = () => t.get('trends/place', {id: 1}, (err, data, res) => {
+const getTrends = () => t.get('trends/place', {id: 1}, (err, chunk, res) => {
   if (err) throw err
-  console.log('Get trends at ' + new Date())
+  const now = new Date()
+  const data = {date: now, chunk: chunk}
+  console.log('Get trends at ' + now)
   fs.appendFileSync('trends.txt', JSON.stringify(data) + '\n')
 })
 
